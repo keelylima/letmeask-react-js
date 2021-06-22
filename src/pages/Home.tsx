fdc4a36ddde1097a4342e044fbd97c4860ef720b
Kelly Lima <keely06@live.com>
2021-06-22 19:30:21 -0300

    add AuthContext and signInWithGoogle function

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,7 @@
 import { useHistory } from 'react-router-dom';
 
+import { auth, firebase}  from '../services/firebase';
+
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg';
